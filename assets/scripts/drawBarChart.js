9537b70a6b0b5653c0136640efd1cd7e64b52d06
alex-hladun <alexhladun@gmail.com>
2020-05-02 00:31:54 +0000

    Added more barchart options and CSS declarations. Updated JSLint minimum version.

--- assets/scripts/drawBarChart.js
+++ assets/scripts/drawBarChart.js
@@ -1,33 +1,62 @@
 let drawBarChart = function(jQuery, data, options, element) {
-  // size the data properly
+  // find the largest data value 
   let barMax = data[0];
   for (let i = 1; i <= data.length - 1; i++) {
     if (data[i] >= barMax) {
       barMax = data[i];
     }
   }
-  console.log(barMax);
 
-  // assign bar percentages
+  // Assign bar values as percentage of the max value
   let barVal = [];
   for (let i = 0; i <= data.length - 1; i++) {
     barVal.push(100*data[i]/barMax+"\%");
   }
-  console.log(barVal);
+  
+  // Calculate bar width percentages. 
+  let barWidth = (100 / (data.length))+"%";
 
-
-
-  console.log(data);
   for (let i = 0; i <= data.length - 1; i++) {
-    console.log("Loop")
-    // generates the bars 
-    let barToMake = $("<div id=\"bar"+i+"\" class=\"bar bar-padding\"><h4>"+data[i]+"</h4></div>");
-    barToMake.css("height",barVal[i]);
-    // adds the bars to the "border" <div> element
+    // Generates the bars, and assigns CSS 
+    let barToMake = $("<div id=\"bar"+i+"\" class=\"bar \"><h4>"+data[i]+"</h4></div>");
+    barToMake.css({
+      "height": barVal[i], 
+      "width": barWidth,
+      "margin-left" : options.barSpacing, 
+      "margin-right" : options.barSpacing,
+      "background" : options.barColour
+    });
+    // Adds the bars to the "border" <div> element (ie chart area)
     barToMake.appendTo("#border");
   }
-  // $("#bar0").css("height","50px")
-  // $("#bar1").css("height","100%")
-  // $("#bar2").css("height","250px")
-  console.log("CSS applied");
+
+  // Assigns CSS to the labels
+  $("h4").css({
+    "font-size" : options.barLabelFontSize,
+    "color" : options.barLabelColour
+    // "justify-content" : center
+  })
+
+  // Assigns CSS to the chart area
+  $("#border").css({
+    "height" : options.height, 
+    "width": options.width,
+    "background" : options.chartBackgroundColour,
+    "border-color" : options.chartBorderColour, 
+    "border-width" : options.borderWidth
+  })
+
+
+  // Assigns CSS to the title. 
+  $("#title").css({
+    "color": options.titleFontColour,
+  })
+
 }
+
+// let options = {width: "900px", height: "500px", barSpacing: "20px", 
+// barLabelPosition: "top", barColour: ["#944BBB"], barLabelColour: ["black"],
+// barLabelFontSize : "10px", 
+// xAxisTitle: "X-Axis Title", yAxisTitle: "Y-Axis Title", yTickCount: 5, 
+// chartTitle: "Chart Title", titleFontSize: 32, titleFontColour: "black",
+// chartBackgroundColour: "#CC92C2", chartBorderColour: "black", borderWidth: "20 px"};
